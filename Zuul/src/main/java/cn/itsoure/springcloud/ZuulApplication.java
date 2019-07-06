b8aa953797821bc52203ce4be9d6b2d61eb94a64
wangqiang <850994281@qq.com>
2019-07-06 11:06:36 +0800

    测试文件

--- Zuul/src/main/java/cn/itsoure/springcloud/ZuulApplication.java
+++ Zuul/src/main/java/cn/itsoure/springcloud/ZuulApplication.java
@@ -16,5 +16,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class);
+		
+		
+		
+		
     }
 }
